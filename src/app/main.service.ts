9746d58672de2ea5494939e2064a709b10b096f7
Riyas Ali MK <riyasalimkokl@gmail.com>
2024-02-04 08:00:33 +0530

    apis updated

--- src/app/main.service.ts
+++ src/app/main.service.ts
@@ -14,19 +14,19 @@ export class MainService {
   }
 
   addTodo(params:any) {
-    return this.http.post(enviroment.back_end + "/add", params)
+    return this.http.post(enviroment.back_end + "add", params)
   }
 
   updateTitle(todo_id: any, params: any) {
-    return this.http.put(enviroment.back_end + "/update_title/" + todo_id, params)
+    return this.http.put(enviroment.back_end + "update_title/" + todo_id, params)
   }
 
   updateStatus(todo_id: string, params: any) {
-    return this.http.put(enviroment.back_end + "/update_status/" + todo_id, params)
+    return this.http.put(enviroment.back_end + "update_status/" + todo_id, params)
   }
 
   deleteTodo(todo_id:any) {
-    return this.http.delete(enviroment.back_end + "/delete_todo/" + todo_id)
+    return this.http.delete(enviroment.back_end + "delete_todo/" + todo_id)
   }
 
 }
